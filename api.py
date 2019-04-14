47be4c88d2a728f138dd105341d6105779100705
Cyrus Cowley <cyficowley@gmail.com>
2019-04-14 14:57:18 -0700

    working initial backend without ros

--- api.py
+++ api.py
@@ -2,14 +2,18 @@ import falcon
 import json
 import base64
 import numpy as np
-import rospy
-from std_msgs.msg import String
+#import rospy
+#from std_msgs.msg import String
 
 
 """ 
 API GUIDE
 
 Uses https://falconframework.org/ as the api handler
+
+To run 
+pip install gunicorn
+gunicorn api:app
 
 PATHS:
 
@@ -18,14 +22,16 @@ POST /arm
 
 """
 
+production = False
+
 
 
 #Gives direction to the drive train
 class DriveTrain(object):
     def __init__(self):
-        self.pub = rospy.Publisher('drive_train', String, queue_size=10)
-        rospy.init_node('drive_train_api', anonymous=True)
-
+        #self.pub = rospy.Publisher('drive_train', String, queue_size=10)
+        #rospy.init_node('drive_train_api', anonymous=True)
+        pass
 
     def on_post(self, req, resp):
         query = falcon.uri.decode(req.query_string)
@@ -49,8 +55,10 @@ class DriveTrain(object):
 
             message = json.dumps(input_data)
 
-            rospy.loginfo(message)
-            self.pub.publish(message)
+            #rospy.loginfo(message)
+            #self.pub.publish(message)
+            print(message)
+
 
             resp.status = falcon.HTTP_200  # This is the default status
             resp.body = (json.dumps({"message":"successfully published"}))
@@ -65,9 +73,9 @@ class DriveTrain(object):
 #Gives endpoints for the arm
 class Arm(object):
     def __init__(self): 
-        self.pub = rospy.Publisher('arm', String, queue_size=10)
-        rospy.init_node('arm_api', anonymous=True)
-
+        #self.pub = rospy.Publisher('arm', String, queue_size=10)
+        #rospy.init_node('arm_api', anonymous=True)
+        pass
 
     def on_post(self, req, resp):
         query = falcon.uri.decode(req.query_string)
@@ -95,8 +103,9 @@ class Arm(object):
 
             message = json.dumps(input_data)
 
-            rospy.loginfo(message)
-            self.pub.publish(message)
+            #rospy.loginfo(message)
+            #self.pub.publish(message)
+            print(message)
 
             resp.status = falcon.HTTP_200  # This is the default status
             resp.body = (json.dumps({"message":"successfully published"}))
@@ -108,12 +117,16 @@ class Arm(object):
 
 
 
-
-# falcon.API instances are callable WSGI apps
-app = falcon.API()
+if(not production):
+    from falcon_cors import CORS
+    public_cors = CORS(allow_all_origins=True, allow_methods_list=["GET","PATCH","POST","DELETE"])
+    app = falcon.API(middleware=[public_cors.middleware])
+else:
+    app = falcon.API()
 
 drive_train = DriveTrain()
 arm = Arm()
 
 app.add_route('/drive_train', drive_train)
 app.add_route('/arm', arm)
+
